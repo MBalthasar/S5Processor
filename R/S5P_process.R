ee60d2d8ee96e496b04fd740dee0c4c603c991eb
MBalthasar <maphi@maxi-dsl.de>
2019-07-09 17:22:12 +0200

    Adjusted description.

--- R/S5P_process.R
+++ R/S5P_process.R
@@ -58,22 +58,22 @@
 #' vnm_shp <- raster::shapefile(system.file(package = "S5Processor",
 #'                              "extdata", "vietnam_borders.shp"))
 #'
-#' # Create vector from both NetCDF files
+#' # Create vector from both NetCDF files.
 #' my_files <- c(x1,x2)
 #'
 #' # Most basic case: provide path to single NetCDF file.
 #' # The user will be promted to choose from a list of product names.
 #' S5P_1 <- S5P_process(input = my_files[1])
 #'
-#' # execute function on several files at once; additionally provide product number.
+#' # Execute function on several files at once; additionally provide product number.
 #' S5P_2 <- S5P_process(input = my_files, product = 39)
 #'
-#' # as above but this time also mask data to aoi and define resolution.
+#' # As above but this time also mask data to aoi and define resolution.
 #' S5P_3 <- S5P_process(input = my_files, my_res = 10000,
 #'                      product = 39, my_aoi = vnm_shp,
 #'                      extent_only = FALSE)
 #'
-#' # this time also apply scale factor to convert units to molecules per cm2.
+#' # This time also apply scale factor to convert units to molecules per cm2.
 #' S5P_4 <- S5P_process(input = my_files, my_res = 10000,
 #'                      product = 39, my_aoi = vnm_shp,
 #'                      extent_only = FALSE,
